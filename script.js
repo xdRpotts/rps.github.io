12b4bd76e74aa04fd60f9ca990925c9bea4bb106
xdRpotts <rpotts1604@gmail.com>
2022-04-11 17:49:25 +0100

    test

--- script.js
+++ script.js
@@ -7,18 +7,16 @@ function rock(){
     console.log(bot[botPick]);
     if (bot[botPick] == 'rock'){
         document.getElementById("win").innerHTML = ("You Won, The Bot Chose: " + bot[botPick]);
-        document.getElementById("disable").disabled = true;
         setTimeout(function(){
             document.getElementById("win").innerHTML = ("");
-            document.getElementById("disable").disabled = false;
+
         }, delay)
     }
     else{
         document.getElementById("lose").innerHTML = ("You Lost, The Bot Chose: " + bot[botPick]);
-        document.getElementByClass("disable").disabled = true;
         setTimeout(function(){
             document.getElementById("lose").innerHTML = ("");
-            document.getElementById("disable").disabled = false;
+
         }, delay)
     }
 }
@@ -27,18 +25,18 @@ function paper(){
     console.log(bot[botPick]);
     if (bot[botPick] == 'paper'){
         document.getElementById("win").innerHTML = ("You Won, The Bot Chose: " + bot[botPick]);
-        document.getElementById("disable").disabled = true;
+
         setTimeout(function(){
             document.getElementById("win").innerHTML = ("");
-            document.getElementById("disable").disabled = false;
+
         }, delay)
     }
     else{
         document.getElementById("lose").innerHTML = ("You Lost, The Bot Chose: " + bot[botPick]);
-        document.getElementByClass("disable").disabled = true;
+
         setTimeout(function(){
             document.getElementById("lose").innerHTML = ("");
-            document.getElementByClass("disable").disabled = false;
+
         }, delay)
     }
 }
@@ -47,18 +45,19 @@ function scissors(){
     console.log(bot[botPick]);
     if (bot[botPick] == 'scissors'){
         document.getElementById("win").innerHTML = ("You Won, The Bot Chose: " + bot[botPick]);
-        document.getElementById("disable").disabled = true;
+
         setTimeout(function(){
             document.getElementById("win").innerHTML = ("");
-            document.getElementById("disable").disabled = false;
         }, delay)
     }   
     else{
         document.getElementById("lose").innerHTML = ("You Lost, The Bot Chose: " + bot[botPick]);
-        document.getElementById("disable").disabled = true;
+        document.getElementsByClassName("scissorsDisable").disabled = true;
+
         setTimeout(function(){
             document.getElementById("lose").innerHTML = ("");
-            document.getElementById("disable").disabled = false;
         }, delay)
     }
 }
+
+
